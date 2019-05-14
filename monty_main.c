3b4f79998cdcc86054125c4d973dbdb3223ffc61
Tu Vo <tuvo1106@gmail.com>
2019-05-14 18:27:28 +0000

    All pseudocode

--- monty_main.c
+++ monty_main.c
@@ -2,17 +2,11 @@
 
 int main(int argc, char **argv)
 {
-	register int count = 0, i = 0;
+	register int i = 0;
 	FILE *monty_file;
 	ssize_t read;
-	char *line;
+	char *line, opcode;
 	size_t len = 0;
-	char c;
-	char **commands;
-	extern stack_t STACK;
-
-	/* global? */
-	(void)STACK;
 
 	/* validate correct num of arguments */
 	if (argc != 2)
@@ -28,33 +22,25 @@ int main(int argc, char **argv)
 		dprintf(2, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-
-	/* get lines */
-	for (c = getc(monty_file); c != EOF; c = getc(monty_file))
-		if (c == '\n')
-			count++;
-	printf("This file has %d lines\n", count);
-	commands = malloc(sizeof(char *) * (count + 1));
-
-	/* read and copy lines */
-	monty_file = fopen(argv[1], "r");
 	while ((read = getline(&line, &len, monty_file)) != -1)
 	{
-		commands[i] = strdup(line);
+		line_number = i + 1;
+		printf("line %i: %s", line_number, line);
+		opcode = line[0] // push
+		argument = line[1] // 4
+		arr =
+			{"push", add_node_push},
+			{"pall", pall_list}
+		if (!valid_command)
+	   		dprintf(2, "L%d: unknown instruction %s\n", line_number, opcode);
+			exit(EXIT_FAILURE);
+		if (valid_argument)
+			push(STACK, argument)
+		else:
+	   		dprintf(2, "L%d: usage: push integer", line_number);
+			exit(EXIT_FAILURE);
 		i++;
 	}
-	commands[i] = NULL;
-
-	/* print lines */
-	for (i = 0; commands[i]; i++)
-	{
-		printf("line %i: %s", i + 1, commands[i]);
-	}
-
-	/* Invalid instruction
-	   dprintf(2, "L%d: unknown instruction %s\n", line_number, opcode);
-	   exit(EXIT_FAILURE);
-	   */
 
 	fclose(monty_file);
 	return (EXIT_SUCCESS);
